c229570814b8fa470aa8eb69990cfca653e8c455
fang <i@liufang.org.cn>
2015-03-12 00:05:59 +0800

    修改程序命名规范化

---
+++ segmentation.h
@@ -0,0 +1,39 @@
+/*
+ *  segmentation
+ *
+ *  license: GPL2
+ *  Copyright (C) i@liufang.org.cn
+ *  Author: fang.liu
+ */
+#ifndef SEG_H_
+#define SEG_H_
+
+#include <stdlib.h>
+#include "str.h"
+#include "index.h"
+
+#define SEGMENTATION_VERSION "1.0"
+
+//匹配结果结构
+typedef struct s_result_node {
+	word* word;
+	struct s_result* next;
+} result_node;
+
+//匹配结果头
+typedef struct s_result {
+	int count;
+	result_node* node;
+	result_node* last;
+} result;
+
+//分词
+result* seg(const char* str);
+void add_result_node(result* result, word* w);
+result* init_result();
+//free result
+void free_result(result* r);
+//打印匹配结果
+void print_result(result* r);
+
+#endif /* SEG_H_ */
